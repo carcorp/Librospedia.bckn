35ca5d78a14f9184f1d37129946b033374103d3c
Jess_V <jessica.puricelli78@gmail.com>
2024-06-29 00:39:50 -0300

    crud changes related to the save,delete process,last changes

--- app/models.py
+++ app/models.py
@@ -1,55 +1,59 @@
+#agrego las clases que se van a asociar a la capa de datos de mi base de datos 
+
 from app.database import get_db
 
-class Book: 
+class Book:
 
-    #Constructor
-    def __init__(self,id_book=None, title=None, language=None, ranking=None, videourl=None, cover=None, author=None):
+    #constructor
+    def __init__(self,id_book=None,title=None, language=None, ranking=None, video_url=None, cover=None, author=None):
         self.id_book = id_book
         self.title = title
-        self.language =language
+        self.language = language
         self.ranking = ranking
-        self.video_url = videourl
+        self.video_url = video_url
         self.cover = cover
         self.author = author
-    
-    #Otro metodo que esta armando
-    #serialize convierte el objeto de la clase 'libro' en un diccionario
-    def serialize(self):
-        return{
+
+    #Otro metodo que está armando
+    #Proceso de conversion de un objeto de la clase libro en un diccionario
+    def serialize(self): #Metodo de la instancia
+        return {
             'id_book': self.id_book,
             'title': self.title,
-            'languague':self.language,
-            'ranking':self.ranking,
-            'video_url':self.video_url,
-            'cover': self.cover, 
-            'author': self.author,
+            'language': self.language,
+            'ranking': self.ranking,
+            'video_url': self.video_url,
+            'cover': self.cover,
+            'author':self.author,
         }
-    @staticmethod #llama de lo generico a lo especifico
-    #Otro metodo
+
+    #Otro metodo +
+    @staticmethod #Llama de lo generico a lo especifico 
     def get_all():
-        # metodo que se encarga de buscar en la base todas las libros 
+        #Logica que se encarga de buscar en la base todas las Libros
         db = get_db()
         cursor = db.cursor()
-        query= "SELECT *FROM books"
+        query = "SELECT * FROM books"
         cursor.execute(query)
-        #obtenga resultados
-        rows =cursor.fetchall()
-        books = [Book(id_book=row[0],title=row[1], language=row[2], ranking=row[3], video_url=row[4], cover=row[5], author=row[6]) for row in rows]
-        #cerramos el cursor 
+        #Obtengo resultados 
+        rows = cursor.fetchall()
+        books = [Book(id_book=row[0],title=row[1], language=row[2], ranking=row[3], video_url=row[4], cover=row[5], author= row[6]) for row in rows]
+        #Cerramos el cursor 
         cursor.close()
         return books
+    
     def save(self):
         db = get_db()
         cursor = db.cursor()
         if self.id_book:
             #Forma de poder insertar ("" 3 comillas string) elementos o variables dentro de una cadena
             cursor.execute("""
-                UPDATE books SET title = %s, language = %s, ranking = %s, video_url = %s, cover = %s, author =%s
+                UPDATE books SET title = %s, language = %s, ranking = %s, video_url = %s, cover = %s, author = %s
                 WHERE id_book = %s
-            """, (self.title, self.language, self.ranking, self.video_url, self.cover, self.author, self.id_book,))
+            """, (self.title, self.language, self.ranking, self.video_url, self.cover, self.author, self.id_book))
         else:
             cursor.execute("""
-                INSERT INTO books (title, language, ranking, video_url,cover, author) VALUES (%s, %s, %s, %s)
+                INSERT INTO books (title, language, ranking, video_url,cover, author) VALUES (%s, %s, %s, %s, %s, %s)
             """, (self.title, self.language, self.ranking, self.video_url, self.cover, self.author))
             #voy a obtener el último id generado
             self.id_book = cursor.lastrowid
@@ -61,20 +65,18 @@ class Book:
         #Logica que se encarga de buscar en la base todas las Libros
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM books WHERE id_book = %s",(book_id))
+        cursor.execute("SELECT * FROM books WHERE id_book = %s",(book_id,))
         #Obtengo resultados 
-        row = cursor.fetchall()
+        row = cursor.fetchone()
         #Cerramos el cursor 
         cursor.close()
         if row:
             return Book(id_book=row[0],title=row[1], language=row[2], ranking=row[3], video_url=row[4], cover=row[5], author=row[6])
-        return None 
-        def delete(self):
-            db = get_db()
-            cursor = db.cursor()
-            cursor.execute("DELETE FROM books WHERE id_book = %s", (self.id_book,))
-            db.commit()
-            cursor.close()
-
-
-
+        return None
+    
+    def delete(self):
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM books WHERE id_book = %s", (self.id_book,))
+        db.commit()
+        cursor.close()
--- run.py
+++ run.py
@@ -1,20 +1,34 @@
+#De la libreria de Flask importame la clase flask ??  
 from flask import Flask
+#Le coloco * para no tener que nombrar todas las rutas
 from app.views import *
+#Llamo a funcion init_app del archivo database
 from app.database import init_app
+#CORS
+from flask_cors import CORS, cross_origin
 
-#inicializacion de la aplicacion con flask
-#Variable especial que se establece automaicamente cuando el archivo está siendo ejecutado. 
-app = Flask(__name__)
+
+#Inicializacion de la aplicacion con flask 
+#Variable especial que se establece automaticamente cuando el archivo esta siendo ejecutado y puede tomar varios valores  
+app = Flask (__name__)
 
 init_app(app)
-#Registrar una ruta asociada a una vista
+#permitir solicitudes desde cualquier origen
+CORS(app, supports_credentials=True)
+
+#Registrar una ruta asociada a una vista 
+#SI no coloco el metodo va a ser por default un GET
 app.route('/api/books/', methods=['GET'])(get_all_books)
 app.route('/api/books/', methods=['POST'])(create_book)
 app.route('/api/books/<int:book_id>', methods=['GET'])(get_book)
 app.route('/api/books/<int:book_id>', methods=['PUT'])(update_book)
 app.route('/api/books/<int:book_id>', methods=['DELETE'])(delete_book)
+cross_origin(supports_credentials=True)
 
-
+#Está condicion va a tomar la variable name el valor main cuando se ejecute el archivo de python 
 if __name__ == '__main__':
-    #Levnta el servidor de desarrollo Flask
+    #Levanta el servidor de desarrollo de flask 
     app.run(debug=True)
+
+
+
